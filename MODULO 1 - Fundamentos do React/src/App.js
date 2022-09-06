63a2305c5f32bc95e85342fd7514783107471136
Marcio Paranhos <marcio.naga@gmail.com>
2022-09-06 18:51:49 -0300

    first commit

---
+++ MODULO 1 - Fundamentos do React/src/App.js
@@ -0,0 +1,15 @@
+// Components
+import SecounudComponent from "./components/SecoundComponent";
+// Style/CSS
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos do React</h1>
+      <SecounudComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ MODULO 1 - Fundamentos do React/src/components/SecoundComponent.js
@@ -0,0 +1,9 @@
+const SecounudComponent = () => {
+  return (
+    <div>
+      <h1>Esse e meu Segundo componente</h1>
+      <p>Tomara que esteja funcionando</p>
+    </div>
+  );
+};
+export default SecounudComponent;
